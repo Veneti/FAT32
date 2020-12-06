48a2d164ca3e7c80427195a5f84f67deb9a061f0
CSE 3320 Developer <devuser@cse3320>
2020-12-05 19:44:39 -0500

    if statements

--- mfs.c
+++ mfs.c
@@ -19,15 +19,76 @@
 
 #define MAX_COMMAND_SIZE 255 // The maximum command-line size
 
+int16_t BPB_BytsPerSec;
+int8_t BPB_SecPerClus;
+int16_t BPB_RsvdSecCnt;
+int8_t BPB_NumFATs;
+int16_t BPB_FATSz32
+
+bool file_isOpen = 0;
+
+int compare(char *cmdString, char *dirString)
+{
+    char dotdot = "..";
+
+    if (strncmp(cmdString, dirString, 2) == 0)
+    {
+        if (strncpy(cmdString, dirString, 2) == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;  
+        }
+    }
+
+    char imgName[12];
+
+    memset(imgName, '\0', 11);
+    strncpy(imgName, dirString, 11);
+
+    char input[12];
+    memset(input, '\0', 12);
+    strncpy(input, cmdString, strlen(cmdString));
+
+    char expandedName[12];
+    memset(expandedName, '\0', 12);
+
+    //removes the .png in the file name
+    char *tokenized = strtok(input, ".");
+    strncpy(expandedName, tokenized, strlen(cmdString));
+
+    tokenized = strtok(NULL, ".");
+
+    if (tokenized)
+    {
+        strncpy( (char*)expandedName+8, tokenized, strlen(token));
+    }
+
+    int i;
+    for (i = 0; i < 11; i++)
+    {
+        expandedName[i] = toupper(expandedName[i]);
+    }
+
+    if (strncmp(expandedName, imgName, 11) == 0)
+    {
+        return 0;
+    }
+
+    return 0;
+}
+
 int main()
 {
+    FILE *filePtr;
+    char *cmd_str = (char *)malloc(MAX_COMMAND_SIZE);
 
-  char *cmd_str = (char *)malloc(MAX_COMMAND_SIZE);
-
-  while (1)
-  {
-    // Print out the mfs prompt
-    printf("mfs> ");
+    while (1)
+    {
+        // Print out the mfs prompt
+        printf("mfs> ");
 
     // Read the command from the commandline.  The
     // maximum command that will be read is MAX_COMMAND_SIZE
@@ -65,16 +126,84 @@ int main()
       token_count++;
     }
 
-    // Now print the tokenized input as a debug check
-    // \TODO Remove this code and replace with your FAT32 functionality
-
-    int token_index = 0;
-    for (token_index = 0; token_index < token_count; token_index++)
-    {
-      printf("token[%d] = %s\n", token_index, token[token_index]);
-    }
-
-    free(working_root);
+     if (token[0] != NULL)
+        {
+            if (strcmp(token[0], "open") == 0)
+            {
+                if (filePtr == NULL)
+                {
+                    filePtr = fopen(token[1], "r");
+                }
+                else
+                {
+                    perror("Error: Improper format. Please put in format open <filename>\n");
+                }
+            }
+            else if (strcmp(token[0], "info") == 0)
+            {
+                // info();
+            }
+            else if (strcmp(token[0], "close") == 0)
+            {
+                if (file_isOpen)
+                {
+                    fclose(filePtr);
+                    
+                    filePtr = NULL;
+                    file_isOpen = 0;
+                }
+                else
+                {
+                    perror("Error: Improper format. Please put in format close <filename>\n");
+                }
+            }
+            else if (strcmp(token[0], "stat") == 0)
+            {
+                if (token[1] != NULL)
+                    // stat(token[1]);
+                else
+                    printf("Error: Improper format. Please put in format stat <filename> or <directory name>\n");
+            }
+            else if (strcmp(token[0], "get") == 0)
+            {
+                if (token[1] != NULL)
+                    // get(token[1]);
+                else
+                    printf("Error: Improper format. Please put in format get <filename>\n");
+            }
+            else if (strcmp(token[0], "cd") == 0)
+            {
+                if (token[1] != NULL)
+                    // cd(token[1]);
+                    //call compare
+                else
+                    printf("Error: Improper format. Please put in format cd <directory>\n");
+            }
+            else if (strcmp(token[0], "ls") == 0)
+            {
+                int i;
+                for (i = 0; i < NUM_ENTRIES; i++)
+                {
+                    char fileName[12];
+                    strncpy()
+                }
+            }
+            else if (strcmp(token[0], "read") == 0)
+            {
+                if (token[1] != NULL && token[2] != NULL && token[3] != NULL)
+                    // read(token[1], atoi(token[2]), atoi(token[3]));
+                else
+                    printf("Error: Improper format. Please put in format read <filename> <position> <number of bytes>\n");
+            }
+            else if (strcmp(token[0], "exit") == 0)
+            {
+                // exit(0);
+            }
+            else
+            {
+                printf("Error: Invalid Command\n");
+            }
+        }
   }
 
   return 0;
